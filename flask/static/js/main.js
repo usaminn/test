55476798df60565c0fd0f4416fc544a2e4af6ba9
usaminn <tuambition0112@gmail.com>
2017-12-16 15:43:55 +0900

    Stop Watchを追加

--- flask/static/js/main.js
+++ flask/static/js/main.js
@@ -1,38 +1,73 @@
 (function() {
   'use strict';
 
+  var timer = document.getElementById('timer');
   var start = document.getElementById('start');
   var stop = document.getElementById('stop');
-  var result = document.getElementById('result');
+  var reset = document.getElementById('reset');
+
   var startTime;
-  var isStarted = false;
+  var elapsedTime = 0;
+  var timerId;
+  var timeToAdd = 0;
+  var isRunning = false;
+
+  function updateTimerText() {
+    var m = Math.floor(elapsedTime / 60000);
+    var s = Math.floor(elapsedTime % 60000 / 1000);
+    var ms = elapsedTime % 1000;
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    ms = ('00' + ms).slice(-3);
+    timer.textContent = m + ':' + s + '.' + ms;
+  }
+
+  function countUp() {
+    timerId = setTimeout(function() {
+      elapsedTime = Date.now() - startTime + timeToAdd;
+      // console.log(elapsedTime);
+      updateTimerText();
+      countUp();
+    }, 10);
+  }
+
+  // start.className = 'btn';
+  // stop.className = 'btn inactive';
+  // reset.className = 'btn inactive';
+
+  function updateButtonState(startButtonState, stopButtonState, resetButtonState) {
+    start.className = startButtonState ? 'btn' : 'btn inactive';
+    stop.className = stopButtonState ? 'btn' : 'btn inactive';
+    reset.className = resetButtonState ? 'btn' : 'btn inactive';
+  }
+
+  updateButtonState(true, false, false);
 
   start.addEventListener('click', function() {
-    if (isStarted === true) {
+    if (isRunning === true) {
       return;
     }
-    isStarted = true;
+    isRunning = true;
+    updateButtonState(false, true, false);
     startTime = Date.now();
-    this.className = 'pushed';
-    stop.className = '';
-    result.textContent = '0.000';
-    result.className = 'standby';
-  });
+    countUp();
+  })
   stop.addEventListener('click', function() {
-    var elapsedTime;
-    var diff;
-    if (isStarted === false) {
+    if (isRunning === false) {
       return;
     }
-    isStarted = false;
-    elapsedTime = (Date.now() - startTime) / 1000;
-    result.textContent = elapsedTime.toFixed(3);
-    this.className = 'pushed';
-    start.className = '';
-    result.className = '';
-    diff = elapsedTime - 5.0;
-    if (Math.abs(diff) < 1.0) {
-      result.className = 'perfect';
-    };
-  });
+    isRunning = false;
+    updateButtonState(true, false, true);
+    clearTimeout(timerId);
+    timeToAdd += Date.now() - startTime;
+  })
+  reset.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+    updateButtonState(true, false, false);
+    elapsedTime = 0;
+    timeToAdd = 0;
+    updateTimerText();
+  })
 })();
